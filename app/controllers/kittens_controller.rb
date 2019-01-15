c9b7978cb4de93e13235a3dfeda83dfe5a8d2808
amyhenning <henning.amy@gmail.com>
2019-01-15 16:21:59 -0700

    Set up controller and routes

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,24 @@
+class KittensController < ApplicationController
+	def index
+		@kittens = Kitten.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def show
+		@kitten = Kitten.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
